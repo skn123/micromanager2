1cf9119c124cc2509b330bc097cc26725731988c
skn123 <skn123@gmail.com>
2015-03-12 15:22:43 +0530

    Scripts

---
+++ cmake/InstallMM.cmake
@@ -0,0 +1,38 @@
+#==============================================================================#
+#                  Gather all files for installation
+#==============================================================================#
+INSTALL( TARGETS ${INSTALL_LIBS}
+         ARCHIVE DESTINATION lib
+         LIBRARY DESTINATION lib
+         RUNTIME DESTINATION bin
+)
+#We keep the directory structure of Micromanager for backward compatibility
+FOREACH(MY_SUBDIR ${MMGR_DIRS})
+  INSTALL(DIRECTORY ${MY_SUBDIR} 
+          DESTINATION include 
+          FILES_MATCHING 
+          PATTERN "*.h" 
+          PATTERN "*.hpp"
+          PATTERN "*.hxx"
+          PATTERN "*.txx"
+  )    
+ENDFOREACH()
+
+#Generate two lists
+SET(MMGr_LIB_Release)
+SET(MMGr_LIB_Debug)
+FOREACH(MyLib ${INSTALL_LIBS})
+  SET(MMGr_LIB_Release ${MMGr_LIB_Release} optimized ${MyLib})
+  SET(MMGr_LIB_Debug ${MMGr_LIB_Debug} debug ${MyLib}_d)
+ENDFOREACH()
+
+#==============================================================================#
+# Configure files with settings for use by the build.
+#==============================================================================#
+CONFIGURE_FILE(${PROJECT_SOURCE_DIR}/cmake/UseMicromanager.cmake.in
+               ${CMAKE_INSTALL_PREFIX}/cmake/UseMicromanager.cmake COPYONLY IMMEDIATE)
+CONFIGURE_FILE(${PROJECT_SOURCE_DIR}/cmake/MicromanagerConfig.cmake.in
+               ${CMAKE_INSTALL_PREFIX}/cmake/MicromanagerConfig.cmake @ONLY IMMEDIATE)
+
+
+
